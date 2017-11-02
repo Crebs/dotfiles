08df7856589ba1c96776989ea63ce87676e5fe44
Riley Crebs <rcrebs@salesforce.com>
2017-11-01 21:39:46 -0700

    Fixing symbolic link to use relative path and prevent infinite linking

--- make.sh
+++ make.sh
@@ -22,6 +22,8 @@ if brew ls --versions neovim > /dev/null; then
     echo "neovim already installed"
 else
     brew install neovim
+    curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs \
+    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
 # Install emacs via Homebrew
 if brew ls --versions emacs > /dev/null; then
@@ -36,31 +38,21 @@ fi
 #########################################
 if [ ! -d "$~/Documents/github/git" ]; then
     echo "Clone git repo"
-    pre=pwd
     cd ~/Documents/github
     git clone git@github.com:git/git.git
-    cd pre
+    cd ~/Documents/github/dotfiles
 fi
 
 ###############################################
 # Dotfiles
 ##############################################
 # variables
-dir=~/.dotfiles                         # dotfiles directory
 olddir=~/dotfiles_old                   # old dotfiles backup directory
-files="bash_profile vimrc vim gitconfig config emacs.d/init.el"      # list of files/folders to symlink in homedir
-
-# move dotfiles to .dotfiles hidden folder
-cp -rf ../dotfiles $dir
+files="bash_profile vimrc vim gitconfig config emacs.d"      # list of files/folders to symlink in homedir
 
 # create dotfiles_old in homedir
 echo "Creating $olddir for backup of any existing dotfiles in ~"
 mkdir -p $olddir
-echo "...done"
-
-# change to the dotfiles directory
-echo "Changing to the $dir directory"
-cd $dir
 echo "...done"
 
 # move any existing dotfiles in homedir to dotfiles_old directory, then create symlinks
@@ -68,7 +60,7 @@ echo "Moving any existing dotfiles from ~ to $olddir"
 for file in $files; do
     mv ~/.$file ~/dotfiles_old/
     echo "Creating symlink to $file in home directory."
-    ln -s $dir/.$file ~/.$file
+    ln -s "$(pwd)"/.$file ~/.$file
 done
 
-ln -s $dir/syspython2/ ~/syspython2
+#ln -s $dir/syspython2/ ~/syspython2
